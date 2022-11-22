fd508123fe2abecdde1f97e06dbf73aa7b1ae6c7
Victor12324 <victor.banes@edu.esiee.fr>
2022-11-22 15:59:39 +0100

    ajout fonction url

--- app/src/main/java/com/example/metetoobo/MainActivity.java
+++ app/src/main/java/com/example/metetoobo/MainActivity.java
@@ -3,12 +3,61 @@ package com.example.metetoobo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    class fetchData extends Thread{
+
+        String data = "";
+
+        public void run(String ville) {
+            try {
+                URL url = new URL("https://api.meteo-concept.com/api/forecast/daily?token=3722d305e101385ebbccdecd7a878d85122bbdd79857766fcfbd2dce06650d2c&name=" + ville);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+
+                while ((line = bufferedReader.readLine()) != null){
+                    data = data + line;
+
+                }
+
+                if (!data.isEmpty()){
+                    JSONObject jsonObject = new JSONObject(data);
+                    JSONArray temperatureMin = jsonObject.getJSONArray("tmin");
+                }
+
+            } catch (MalformedURLException e){
+                e.printStackTrace();
+            } catch (IOException e){
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
     }
 }
 
