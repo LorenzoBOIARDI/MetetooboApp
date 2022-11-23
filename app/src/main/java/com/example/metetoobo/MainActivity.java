686702c465e0338fe3108865ffe86622c4ecfd4e
Victor12324 <victor.banes@edu.esiee.fr>
2022-11-23 13:20:34 +0100

    ajout fonction url

--- app/src/main/java/com/example/metetoobo/MainActivity.java
+++ app/src/main/java/com/example/metetoobo/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,63 +25,23 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-        private Button btnFetchData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        initView();
+
     }
 
-    private void initView() {
-        btnFetchData = findViewById(R.id.btn_fetchData);
+    public void toMeteo(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, MeteoActivity.class);
+        startActivity(intent);
 
-        btnFetchData.setOnClickListener(view -> {
-            fetchData test = new fetchData();
-            test.runFetch("Rennes");
-        });
     }
 
 
-    class fetchData extends Thread{
-
-        String data = "";
-
-        public void runFetch(String ville) {
-            try {
-                URL url = new URL("https://api.meteo-concept.com/api/forecast/daily?token=3722d305e101385ebbccdecd7a878d85122bbdd79857766fcfbd2dce06650d2c&name=" + ville);
-                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-                InputStream inputStream = httpURLConnection.getInputStream();
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-                String line;
-
-                while ((line = bufferedReader.readLine()) != null){
-                    data = data + line;
-
-                }
-
-                if (!data.isEmpty()){
-                    JSONObject jsonObject = new JSONObject(data);
-                    JSONArray temperatureMin = jsonObject.getJSONArray("tmin");
-                    String tempMinString = temperatureMin.toString();
-                    TextView textView = findViewById(R.id.text_showTempMin);
-                    textView.setText(tempMinString);
-
-                }
-
-            } catch (MalformedURLException e){
-                e.printStackTrace();
-            } catch (IOException e){
-                e.printStackTrace();
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
 
 
-
-    }
 }
 
---
+++ app/src/main/java/com/example/metetoobo/MeteoActivity.java
@@ -0,0 +1,83 @@
+package com.example.metetoobo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MeteoActivity extends AppCompatActivity {
+
+    private Button btnFetchData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meteo);
+        Intent intent = getIntent();
+        //initView();
+    }
+
+/*
+    private void initView() {
+        btnFetchData = findViewById(R.id.btn_fetchData);
+
+        btnFetchData.setOnClickListener(view -> {
+            fetchData test = new fetchData();
+            test.runFetch("Rennes");
+        });
+    }
+
+    class fetchData extends Thread{
+
+        String data = "";
+
+        public void runFetch(String ville) {
+            try {
+                URL url = new URL("https://api.meteo-concept.com/api/forecast/daily?token=3722d305e101385ebbccdecd7a878d85122bbdd79857766fcfbd2dce06650d2c&name=" + ville);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+
+                while ((line = bufferedReader.readLine()) != null){
+                    data = data + line;
+
+                }
+
+                if (!data.isEmpty()){
+                    JSONObject jsonObject = new JSONObject(data);
+                    JSONArray temperatureMin = jsonObject.getJSONArray("tmin");
+                    String tempMinString = temperatureMin.toString();
+                    TextView textView = findViewById(R.id.text_showTempMin);
+                    textView.setText(tempMinString);
+
+                }
+
+            } catch (MalformedURLException e){
+                e.printStackTrace();
+            } catch (IOException e){
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
+    }
+
+ */
+}
