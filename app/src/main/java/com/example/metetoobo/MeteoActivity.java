4f77a4c78e6a6aac367b39f36b4d328610d7e610
Victor12324 <victor.banes@edu.esiee.fr>
2022-11-30 19:29:33 +0100

    ajout fonction url

--- app/src/main/java/com/example/metetoobo/MeteoActivity.java
+++ app/src/main/java/com/example/metetoobo/MeteoActivity.java
@@ -4,8 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -21,29 +29,61 @@ import java.net.URL;
 
 public class MeteoActivity extends AppCompatActivity {
 
-    private Button btnFetchData;
-    private String tempMinString;
+    private TextView text_showTempMin;
+    private RequestQueue mQueue;
+
+    public MeteoActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meteo);
-        Intent intent = getIntent();
-        initView();
-    }
+        text_showTempMin = findViewById(R.id.text_showTempMin);
+        Button buttonFetchURL = findViewById(R.id.btn_fetchData);
 
+        mQueue = Volley.newRequestQueue(this);
 
-    private void initView() {
-        btnFetchData = findViewById(R.id.btn_fetchData);
-
-        btnFetchData.setOnClickListener(view -> {
-            URLFetchDataIntentService test = new URLFetchDataIntentService();
-            //tempMinString = test.urlFetcher("Rennes");
-            TextView textView = findViewById(R.id.text_showTempMin);
-            textView.setText(tempMinString);
-            //textView.setText("test1");
+        buttonFetchURL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                jsonParse();
+            }
         });
     }
+
+    private void jsonParse() {
+
+        String url = "https://api.meteo-concept.com/api/forecast/daily/0?token=3722d305e101385ebbccdecd7a878d85122bbdd79857766fcfbd2dce06650d2c&search=Rennes";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("forecast");
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject forecast = jsonArray.getJSONObject(i);
+
+                                String tempMin = forecast.getString("tmin");
+
+                                text_showTempMin.append(tempMin);
+                            }
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        mQueue.add(request);
+    }
+
 /*
     class FetchData extends Thread{
 
@@ -55,7 +95,7 @@ public class MeteoActivity extends AppCompatActivity {
         public void runFetch(String ville) {
             try {
                 //URL url = new URL("https://api.meteo-concept.com/api/forecast/daily?token=3722d305e101385ebbccdecd7a878d85122bbdd79857766fcfbd2dce06650d2c&search=" + ville);
-                URL url = new URL("https://api.meteo-concept.com/api/forecast/daily?token=3722d305e101385ebbccdecd7a878d85122bbdd79857766fcfbd2dce06650d2c&search=Rennes");
+                URL url = new URL("https://api.meteo-concept.com/api/forecast/daily/0?token=3722d305e101385ebbccdecd7a878d85122bbdd79857766fcfbd2dce06650d2c&search=Rennes");
                 HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
                 InputStream inputStream = httpURLConnection.getInputStream();
                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
