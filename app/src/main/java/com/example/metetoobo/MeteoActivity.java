067d4dc294ac43484a296e012f2d716a9fb14b3c
Victor12324 <victor.banes@edu.esiee.fr>
2022-11-30 20:20:36 +0100

    URL ok

--- app/src/main/java/com/example/metetoobo/MeteoActivity.java
+++ app/src/main/java/com/example/metetoobo/MeteoActivity.java
@@ -1,9 +1,7 @@
 package com.example.metetoobo;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,17 +13,8 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class MeteoActivity extends AppCompatActivity {
 
@@ -61,6 +50,7 @@ public class MeteoActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
+                            /*
                             JSONArray jsonArray = response.getJSONArray("forecast");
                             for (int i = 0; i < jsonArray.length(); i++) {
                                 JSONObject forecast = jsonArray.getJSONObject(i);
@@ -68,7 +58,21 @@ public class MeteoActivity extends AppCompatActivity {
                                 String tempMin = forecast.getString("tmin");
 
                                 text_showTempMin.append(tempMin);
-                            }
+
+
+                            JSONObject temp = response.getJSONObject("tmin");
+                            String tempMin = temp.toString();
+                            text_showTempMin.append(tempMin);
+*/
+                            JSONObject jsonObject2 = response.getJSONObject("city");
+                            String city = jsonObject2.getString("name");
+
+                            JSONObject jsonObject1 = response.getJSONObject("forecast");
+                            String tempMin = jsonObject1.getString("tmin");
+                            String tempMax = jsonObject1.getString("tmax");
+
+                            text_showTempMin.append("Ville de " + city+ " :\n" + "temp min :" + tempMin + "\n" + "temp max :" + tempMax);
+
 
 
                         } catch (JSONException e) {
